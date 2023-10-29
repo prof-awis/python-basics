ef365e6a4455626655f5f891aab6b051cb40db48
prof-awis <awinoemanuel2000@gmail.com>
2023-10-29 05:26:08 +0300

    Update in the documentation

--- python-basics/basic_calculator.py
+++ python-basics/basic_calculator.py
@@ -1,8 +1,15 @@
 first = input("First: ")
 second = input("Second: ")
 
-# sum1 = int(first) + int(second)
-sum2 = float(first) + float(second)
+sum1 = int(first) + int(second)
+product1 = int(first) * int(second)
+division1 = int(first) / int(second)
 
-# print(f"The sum is: {sum1}")
-print("Sum: " + str(sum2))
+
+# sum2 = float(first) + float(second)
+# print("Sum: " + str(sum2))
+
+print(f"The sum is: {sum1}")
+print(f"The product is: {product1}")
+print(f"The division is: {division1}")
+
